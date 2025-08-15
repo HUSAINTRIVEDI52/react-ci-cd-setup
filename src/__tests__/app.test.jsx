b06e378819c624561d6a19241ee2f80d075973f7
Husain <husaintd52@gmail.com>
2025-08-15 19:40:05 +0530

    Added github/workflow code

---
+++ src/__tests__/app.test.jsx
@@ -0,0 +1,23 @@
+import {fireEvent, render,screen} from '@testing-library/react'
+
+import App from '../App'
+import { expect } from 'vitest';
+
+test('increments count on button click', () => {
+  render(<App />);
+  
+  const buttonElement = screen.getByText(/count is 0/i);
+  expect(buttonElement).toBeInTheDocument();
+
+  // 1st click → expect count is 1
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveTextContent('count is 1');
+
+  // 2nd click → expect count is 2
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveTextContent('count is 2');
+
+  // 3rd click → expect count is 3
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveTextContent('count is 3');
+});
